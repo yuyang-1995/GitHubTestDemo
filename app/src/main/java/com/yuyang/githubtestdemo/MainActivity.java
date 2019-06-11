d7978adc5ee50c035daa81d933b1f404e2ab00e0
yuy <yuy@jrweid.com>
2019-06-11 18:10:42 +0800

    Commit

--- app/src/main/java/com/yuyang/githubtestdemo/MainActivity.java
+++ app/src/main/java/com/yuyang/githubtestdemo/MainActivity.java
@@ -19,10 +19,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         tv = findViewById(R.id.tv);
         btn = findViewById(R.id.btn);
 
+        tv.setOnClickListener(this);
+        btn.setOnClickListener(this);
+
     }
 
     @Override
     public void onClick(View view) {
+
+        switch (view.getId()){
+
+
+        }
 
     }
 }
