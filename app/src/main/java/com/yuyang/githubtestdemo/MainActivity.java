b48d14955c6fb0e1a2cd7f63c81167920ac2d9af
yuy <yuy@jrweid.com>
2019-06-11 16:59:51 +0800

    First Commit

--- app/src/main/java/com/yuyang/githubtestdemo/MainActivity.java
+++ app/src/main/java/com/yuyang/githubtestdemo/MainActivity.java
@@ -3,8 +3,12 @@ package com.yuyang.githubtestdemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+   private TextView tc;
+   private Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
