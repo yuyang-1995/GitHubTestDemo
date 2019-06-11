7130ec225b655eae292b32e11dd2d4078e34ec59
yuy <yuyang_keep@163.com>
2019-06-11 23:00:35 +0800

    Commit 6/11/2300

--- app/src/main/java/com/yuyang/githubtestdemo/MainActivity.java
+++ app/src/main/java/com/yuyang/githubtestdemo/MainActivity.java
@@ -24,6 +24,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View view) {
 
+        switch (view.getId()){
+
+
+
+        }
+
 
 
 
